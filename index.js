811bc3835ba09f409c823854960d98d8cf25aca7
milenegj <milene.jannetti@gmail.com>
2022-06-17 14:48:35 -0500

    Get tweets OK

--- index.js
+++ index.js
@@ -37,6 +37,32 @@ app.post("/tweets", (req, res) => {
     }
 });
 
-
+app.get("/tweets", (req, res) => {
+    let limitShown = 10
+    if (AllTweets.length > 0) {
+        let identifiedUser
+        let identifiedTT = AllTweets.map(function (t) {
+            identifiedUser = userData.filter(user => user.username === t.username);
+            console.log(identifiedUser)
+            return ({
+                username: identifiedUser[0].username,
+                avatar: identifiedUser[0].avatar,
+                tweet: t.tweet
+            })
+        })
+        if (identifiedTT.length <= limitShown) {
+            res.send(identifiedTT);
+        } else {
+            let last = identifiedTT.length - 1
+            let exhibitedTweets = [];
+            for (let i = 1; i <= limitShown; i++) {
+                exhibitedTweets.push(identifiedTT[(last - (limitShown-i))]);
+            }
+            res.send(exhibitedTweets);
+        }
+    } else {
+        res.send(AllTweets);
+    }
+})
 
 app.listen(5000);
